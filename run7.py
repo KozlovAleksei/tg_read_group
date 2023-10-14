d4aba953fad0d60604d7011f7d1327c87d3d7d32
Aleksei Kozlov <awkozlov.mba@gmail.com>
2023-10-14 04:39:06 +0300

    сделан вывод в файл без спецсимволов

--- run7.py
+++ run7.py
@@ -8,22 +8,15 @@ from telethon.tl.functions.messages import UpdatePinnedMessageRequest
 from telethon.tl.functions.messages import SendMessageRequest
 from telethon.tl.functions.messages import SendVoteRequest
 from telethon.tl import types
-
+from datetime import datetime
 import time
+import mlb
 
-def read_text_from_file(TextFileName):
-    for _ in range(3):
-        try:
-            with open(TextFileName, "r") as file:
-                text = file.read()
-                return text
-        except FileNotFoundError:
-            time.sleep(0.01)
-    return ''
+api_id = int(mlb.read_text_from_file('C:\\py386\secret\\001\\api_id.txt'))
+api_hash = mlb.read_text_from_file('C:\\py386\secret\\001\\api_hash.txt')
+phone = mlb.read_text_from_file('C:\\py386\secret\\001\\phone.txt')
+message_file = 'C:\\py386\\message.txt'
 
-api_id = int(read_text_from_file('C:\\py386\secret\\001\\api_id.txt'))
-api_hash = read_text_from_file('C:\\py386\secret\\001\\api_hash.txt')
-phone = read_text_from_file('C:\\py386\secret\\001\\phone.txt')
 
 client = TelegramClient('session_name', api_id, api_hash)
 client.connect()
@@ -54,13 +47,13 @@ while True:
         last_chat = result.chats[-1]
         offset_id = last_chat.id  # обновление смещения для следующей пагинации
 
-# target_chat_id = 1772781939  # https://web.telegram.org/a/#-1001772781939 'Олег торгует'
+target_chat_id = 1772781939  # https://web.telegram.org/a/#-1001772781939 'Олег торгует'
 # target_chat_id = 1734066007  # https://web.telegram.org/a/#-1001734066007 'БКС Мир инвестиций'
-target_chat_id = 1322987562  # https://web.telegram.org/a/#-1001322987562 'Atusoi'
+# target_chat_id = 1322987562  # https://web.telegram.org/a/#-1001322987562 'Atusoi'
 
 for i, chat in enumerate(chats):
     try:
-        # print(i+1, chat.id, '~', chat.title, '~', chat.username)
+        print(i+1, chat.id, '~', chat.title, '~', chat.username)
         if chat.id == target_chat_id:
             groups.append(chat)
             break
@@ -69,38 +62,48 @@ for i, chat in enumerate(chats):
 
 try:
     target_group = groups[0]
-    print(target_group)
 except Exception:
     print(target_chat_id, ' надо закрепить на главной закладке Телеграм')
-# print(target_group)
+    breakpoint()
 
-# print("Group ID:", target_group.id)
-# print("Title:", target_group.title)
-# print("Username:", target_group.username)
+print(target_group)
+print("Group ID:", target_group.id)
+print("Title:", target_group.title)
+print("Username:", target_group.username)
 
-messages = client.get_messages(target_group, limit=1)
+while True:
+    time.sleep(1)
+    messages = client.get_messages(target_group, limit=1)  # для сообщений
 
-last_message = client.get_messages(target_group, limit=1)[0]
+    last_message = client.get_messages(target_group, limit=1)[0]  # для голосования
 
-if isinstance(last_message.media, MessageMediaPoll):
-    poll = last_message.media.poll
-    print("ГОЛОСОВАНИЕ")
-    print(poll.question)
+    if isinstance(last_message.media, MessageMediaPoll):
+        poll = last_message.media.poll
+        print("ГОЛОСОВАНИЕ")
+        print(poll.question)
 
-    options = []
-    for answer in poll.answers:
-        print(answer.text)
+        options = []
+        for answer in poll.answers:
+            print(answer.text)
 
-        if answer.text == 'Не знаю':  # 'Не согласен' 'Согласен' 'Не знаю'
-            options = [answer.option]  # Создание списка options с одним элементом
-            break
+            if answer.text == 'Не знаю':  # 'Не согласен' 'Согласен' 'Не знаю'
+                options = [answer.option]  # Создание списка options с одним элементом
+                break
 
-    if len(options) > 0:
-        # Отправка голоса
-        client(SendVoteRequest(target_group.id, last_message.id, options=options))
-else:
-    print("Сообщение")
-    last_message = messages[0].message
-    print(last_message)
-    if last_message !="Занято":
-        client(SendMessageRequest(target_chat_id, "Занято"))
+        if len(options) > 0:
+            # Отправка голоса
+            client(SendVoteRequest(target_group.id, last_message.id, options=options))
+    else:
+        print("Сообщение")
+        print(messages[0])
+
+        last_message = datetime.now().strftime("%d.%m.%Y %H:%M:%S") + '\n'
+        last_message = last_message + messages[0].message
+        print(last_message)
+        with open(message_file, 'w', encoding='cp1251', errors='ignore') as file:
+            file.write(last_message)
+        # breakpoint()
+        # if last_message !="Занято":
+        #     client(SendMessageRequest(target_chat_id, "Занято"))
+
+
